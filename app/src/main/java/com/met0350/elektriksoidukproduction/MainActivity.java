94ec484acee19b72a7bf93cd2dfe399fa85544b6
ignatj <ignatjasevits@gmail.com>
2024-03-12 19:18:19 +0200

    - Create: menu with mock buttons, window to show action according to menu, indicators panel

---
+++ app/src/main/java/com/met0350/elektriksoidukproduction/MainActivity.java
@@ -0,0 +1,14 @@
+package com.met0350.elektriksoidukproduction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
